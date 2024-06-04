eb5da4a0978355d7392c154ad00305bebc436ef2
Tiffany Tabourne <tiffanytabourne@tiffanys-mbp.lan>
2024-06-03 21:38:30 -0700

    fix: remove analytics

--- site/scripts.js
+++ site/scripts.js
@@ -1,24 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDJ0ZGZ8qGK03aX7xFPQj2dSFvTSbNvwWE",
-  authDomain: "forthegirls-c4ef4.firebaseapp.com",
-  projectId: "forthegirls-c4ef4",
-  storageBucket: "forthegirls-c4ef4.appspot.com",
-  messagingSenderId: "661727640182",
-  appId: "1:661727640182:web:9d38567400b66fcd093d57",
-  measurementId: "G-L84HD9Z1XJ"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-
 function calculateWPK() {
     console.log("Button clicked");
     const power = document.getElementById('power').value;
