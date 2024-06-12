7e545be907f890ad8c904e0ae2a69693ae970a77
ttabou_nike <tiffany.tabourne@nike.com>
2024-06-12 13:31:39 -0700

    feat: add pounds to kg

---
+++ site/utility/calculateWPKG.js
@@ -0,0 +1,35 @@
+
+function calculateWPKG() {
+    console.log("Button clicked - calculateWPKG()");
+    const power = document.getElementById('power').value;
+    const pounds = document.getElementById('weight').value;
+
+    kilograms = convertPoundsToKilograms(pounds);
+
+
+    console.log("Power:", power);
+    console.log("Weight in kg:", kilograms);
+
+    if (power && kilograms) {
+        const ftp = power;
+        const wpk = (power / kilograms).toFixed(2);
+        console.log(power/kilograms);
+
+        document.getElementById('wpkResult').textContent = `Your WPK is: ${wpk} W/kg`;
+    } else {
+        alert('Please enter both your FTP and weight.');
+    }
+}
+
+function convertPoundsToKilograms(pounds){
+    console.log("Weight in lbs: ", pounds);
+
+    if (pounds) {
+        const kg = (pounds / 2.2046).toFixed(2);
+        return kg
+    }
+
+    return 0
+}
+
+console.log("calulateWPK.js is loaded");
---
+++ tests/calculateWPK.test.js
@@ -0,0 +1,5 @@
+const sum = require('../site/utility/calculateWPKG.js');
+
+test('Calculates watts per kilogram', () => {
+  true
+});
